377dd8388e2a036438b07c0866d3a63b71a26cca
julianbrom1 <bec21001@byui.edu>
2025-01-23 12:08:59 -0500

    BOM

--- week02/scripts/scripts.js
+++ week02/scripts/scripts.js
@@ -1,9 +1,21 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+buttonElement.addEventListener('click', function(){
+    if(input.value.trim() !== '') {
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector('ul');
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);}
+})
+
+deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+})
+
+input.value = '';
+
+input.focus();
