f57a6a999f6a832c2d450714e728c5de2271cb50
julianbrom1 <bec21001@byui.edu>
2025-02-07 15:43:42 -0500

    Done

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,12 @@
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
 document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+const temperature = 5;
+const wind = 18;
+
+function calculateWindChill(temperature, wind) {
+    return (13.12 + 0.6215 * temperature - 11.37 * wind ** 0.16 + 0.3965 * temperature * wind ** 0.16).toFixed();
+}
+
+document.getElementById('windChillResult').innerHTML = ` ${calculateWindChill(temperature, wind)}`;
